e5369a71ebb09a3aca4414abeb9b9b2827bc0fe8
Oleh Hinko <vipginko99@gmail.com>
2019-03-28 22:30:05 +0200

    second commit

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -13,31 +13,50 @@ const DialogItem = (props) => {
 
 const Message = (props) => {
   return (
-    <div className="message">{props.message}</div>
+    <div className="message">{props.name}</div>
   )
 }
 
-
 const Dialogs = (props) => {
-  return (
-    <div className="dialogs">
-      <div className="dialogs-items">
-        <DialogItem name="Dimych" id="1" />
-        <DialogItem name="Andrey" id="2" />
-        <DialogItem name="Sasha" id="3" />
-        <DialogItem name="Sveta" id="4" />
-        <DialogItem name="Victor" id="5" />
-        <DialogItem name="Valera" id="6" />
-      </div>
-      <div className="messages">
-        <Message message="Hi" id="1" />
-        <Message message="How is you" id="1" />
-        <Message message="Privit" id="1" />
-        <Message message="Privit" id="1" />
-        <Message message="Privit" id="1" />
-      </div>
-    </div>
-  );
-}
+      let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sasha'},
+        {id: 4, name: 'Sveta'},
+        {id: 5, name: 'Victior'},
+        {id: 6, name: 'Valera'}
+      ]
+
+      let messageData = [
+        {id: 1,message: 'Hi'},
+        {id: 2, message: 'How is you'},
+        {id: 3, message: 'What do you do'},
+        {id: 4, message: 'Pruvit'},
+        {id: 5, message: 'Hi'}
+      ]
+
+      let dialogsElements = dialogsData.map((dialog) => {
+        return (
+          <DialogItem name={dialog.name} id={dialog.id} />
+        );
+      })
+
+      let messagesElements = messageData.map((message) => {
+        return (
+          <Message name={message.message} />
+        );
+      })
+      return (
+        <div className="dialogs">
+          <div className="dialogs-items">
+            {dialogsElements}
+          </div>
+          <div className="messages">
+            {messagesElements}
+          </div>
+        </div>
+      );
+    }
+
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -2,20 +2,29 @@ import React from 'react';
 import './MyPosts.scss';
 import Post from './Post/Post';
 const MyPosts = () => {
+  let postData = [
+    {id: 1, message: 'Hi, how are you', likesCount: 10},
+    {id: 2, message: 'Hi, my first post', likesCount: 20}
+  ]
+
+  let postElements = postData.map((post) => {
+    return (
+        <Post message={post.message} like={post.likesCount} />
+    );
+  });
   return (
     <div className="myposts">
         <h3>My post</h3>
         <div>
            <div>
-              <textarea></textarea>
+              <textarea placehholder="Hi"></textarea>
             </div>
             <div>
               <button>Add post</button>
             </div>
         </div>
         <div>
-            <Post message="Hi, how are you" like="10" />
-            <Post message="Hi, my first post" like="20" />
+          {postElements}
         </div>
     </div>
   );
